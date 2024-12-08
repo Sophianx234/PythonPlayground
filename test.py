984e358e0e5808938d52b59ed1e15dfd76dcbd6a
Damian X <dx4336969@gmail.com>
2024-12-08 14:22:46 +0000

    practice 1

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print('Hello world Bro'+' Damian')
+items = ['Avocado','Pear','Pomegrade','Peaches','Tangerine']
+items.append('Pineapple')
+print(items)
+
+animeChar = ['Boruto','Deku','Bakugo','Sarada','Saitama','Kankuro']
+
+emptyList = [x for x in range(11) if x%2 ==1]
+
+print(emptyList)
+
+
+
