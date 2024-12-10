9cf3d1f5f0dfbda2ecac8af2910189be7471ae7f
Damian X <dx4336969@gmail.com>
2024-12-10 21:06:52 +0000

    working on treasure island

--- treasure.py
+++ treasure.py
@@ -12,21 +12,23 @@ treasureIslandArt = """
 
 print(treasureIslandArt)
 
-print('Welcome to the Island.\nYour mission is to find the treasure')
-path = input("Choose a path: left or right ? ")
+print('Welcome to Treasure Island.')
+print('Your mission is to find the treasure.')
+path = input('You\'re at a crossroad, where do you want to go? Type "left" or "right" ')
 
-if path.lower() =='left':
-    path = input('Swim or wait')
-    if path.lower() == 'wait':
-        path = input('Which door? Blue or Red?')
-    else :
-        print("Attacked by trout.\nGame Over")
-        if path.lower() == 'blue': 
-            print('Eaten by beasts.\nGame Over')
-        elif path.lower() == 'red':
-            print('Burned by fire.\nGame Over')
-        elif path.lower() =='yellow':
-            print('You Win!')
-        else: 
-            print('Game Over')
-else: print("Fall into a hole.\nGame Over")
+if path.lower() == 'right':
+    print('Fall into a hole.\nGame Over')
+if path.lower() == 'left':
+    path = input('There is water? swim or wait? ')
+if path.lower() == 'swim':
+    print('Attacked by trout.\nGameover')
+if path.lower() == 'wait':
+    path = input('There are doors in the room which one do you want? Red, Blue or Yellow? ')
+    if path.lower() == 'red':
+        print("Burned by fire.\n Game Over")
+    elif path.lower() == 'blue':
+        print("Eaten by beasts.\nGame Over")
+    elif path.lower() == 'yellow':
+        print('You win!')
+    else: 
+        print("Game Over")
