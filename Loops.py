0bebc162bef2acf00fdbbb56abc831a34037fc68
Damian X <dx4336969@gmail.com>
2024-12-10 22:59:06 +0000

    implementing for loop

---
+++ Loops.py
@@ -0,0 +1,22 @@
+
+
+friends = ['Boruto',"Naruto", 'Sasuke']
+for x in range(len(friends)):
+    print(x)
+    
+newFriends = [x for x in friends if x !='Boruto' ]
+print(newFriends)
+
+print( 'Dormamu' in friends)
+
+scores = [66,45,13,11,12,85,39,90,77,18,89,93]
+maxScore = 0
+for x in scores:
+    if maxScore<x:
+        maxScore = x
+    else:
+        maxScore = maxScore
+
+print(maxScore)
+
+print(max(scores))
