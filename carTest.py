df79ea5fed0ea0cbd44576adbf2e09afdc5423b7
Damian X <dx4336969@gmail.com>
2024-12-13 21:22:47 +0000

    adding oop class

--- carTest.py
+++ carTest.py
@@ -7,3 +7,15 @@ car1.drive()
 car1.drive()
 car1.drive()
 print(car1.accelerate)
+
+
+class Student: 
+    class_year = 2080 
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def greet(self):
+        print(f"Hello {self.name}")
+    
+        
+print(Student.class_year)
