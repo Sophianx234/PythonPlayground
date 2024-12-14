ce803434a14060f5e7d5597814469bc4324525ec
Damian X <dx4336969@gmail.com>
2024-12-14 08:07:01 +0000

    employing classes

--- abstractClasses.py
+++ abstractClasses.py
@@ -32,10 +32,38 @@ class Circle(Shape):
     def calcArea (self):
         return 3.142*self.radius**2
     def describe(self):
-        print(f'it is a circle with an area of {self.calcArea()}cm^2')
+        print(f'it is a circle with an area of {self.calcArea()}cm')
         super().describe()
     
 circle1 = Circle(True,'Blue',2.5)
-
+ 
 print(circle1.calcArea())
 circle1.describe()
+
+class Library:
+    def __init__(self,name):
+        self.name = name
+        self.books = []
+    def add_books(self,book):
+        self.books.append(book)
+    def list_books(self):
+        return [f'{book.name}' for book in self.books]
+        
+class Book:
+    def __init__(self,name):
+        self.name = name
+ 
+ 
+book1 = Book('Harry Potter xvx')       
+book2 = Book('Fantastic Mr. Fox')       
+book3 = Book('Shaolin Monks')       
+book4 = Book('The intellingent Monger')  
+
+library1 = Library('Northern X insight ')  
+
+library1.add_books(book1)   
+library1.add_books(book2)   
+library1.add_books(book3)   
+library1.add_books(book4)
+
+print(library1.list_books() )  
